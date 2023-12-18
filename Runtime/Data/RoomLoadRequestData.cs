ee399370b28f52e4a2e60bb9edf2599f002a86a5
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-18 12:06:55 +0600

    Merge pull request #1 from Studio-23-xyz/dev
    Initial version of package

---
+++ Runtime/Data/RoomLoadRequestData.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Data
+{
+    [Serializable]
+    public struct RoomLoadRequestData
+    {
+        public RoomData RoomToLoad;
+        public LoadSceneMode LoadSceneMode;
+        public bool ActivateOnLoad;
+        public int Priority;
+
+        public RoomLoadRequestData(RoomData roomToLoad = null)
+        {
+            RoomToLoad = roomToLoad;
+            LoadSceneMode = LoadSceneMode.Additive;
+            ActivateOnLoad = true;
+            Priority = 100;
+        }
+    }
+}

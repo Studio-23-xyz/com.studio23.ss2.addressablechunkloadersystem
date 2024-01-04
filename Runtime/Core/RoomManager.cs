6f77f4f8a95872216f04305958395ad460c782bb
B-DeshiDev <ffalsoy@gmail.com>
2024-01-04 12:01:52 +0600

    Merge pull request #7 from Studio-23-xyz/dev
    Remove assetreference<Sceneasset> due to build errors + minor fixes

---
+++ Runtime/Core/RoomManager.cs
@@ -0,0 +1,351 @@
+using System;
+using System.Collections.Generic;
+using Bdeshi.Helpers.Utility;
+using Cysharp.Threading.Tasks;
+using NaughtyAttributes;
+using Studio23.SS2.AddressableChunkLoaderSystem.Data;
+using UnityEngine;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Core
+{
+    [RequireComponent(typeof(RoomLoader))]
+    public class RoomManager:MonoBehaviourSingletonPersistent<RoomManager>
+    {
+        [SerializeField] List<FloorData> _allFloors;
+        private RoomLoader _roomLoader;
+        public RoomLoader  RoomLoader=> _roomLoader;
+        
+        public event Action<FloorData> OnFloorEntered;
+        public event Action<FloorData> OnFloorExited;
+
+        /// <summary>
+        /// Fired when room entered and loaded
+        /// NOTE: DOES NOT FIRE IF THE ROOM'S FLOOR IS NOT IN _allFloors
+        /// EX: STAIRS
+        /// DIRECTLY SUB TO THE SCRIPTABLE IN SUCH CASES
+        /// </summary>
+        public event Action<RoomData> OnRoomEntered;
+
+        /// <summary>
+        /// Fired when room entered and loaded
+        /// NOTE: DOES NOT FIRE IF THE ROOM'S FLOOR IS NOT IN _allFloors
+        /// EX: STAIRS
+        /// DIRECTLY SUB TO THE SCRIPTABLE IN SUCH CASES
+        /// </summary>
+        public event Action<RoomData> OnRoomExited;
+
+        public RoomData CurrentEnteredRoom => _currentEnteredRoom;
+        [Required] [SerializeField] private RoomData _currentEnteredRoom;
+
+        [ShowNativeProperty]
+        public FloorData CurrentFloor => _currentEnteredRoom ? _currentEnteredRoom.Floor : null;
+
+        public Transform Player;
+        
+        protected override void Initialize()
+        {
+            _roomLoader = GetComponent<RoomLoader>();
+
+            foreach (var floor in _allFloors)
+            {
+                floor.Initialize();
+
+                floor.OnFloorEntered += OnFloorEntered;
+                floor.OnFloorExited += OnFloorExited;
+
+                foreach (var roomData in floor.RoomsInFloor)
+                {
+                    roomData.OnRoomEntered += OnRoomEntered;
+                    roomData.OnRoomExited += OnRoomExited;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var floor in _allFloors)
+            {
+                floor.OnFloorEntered -= OnFloorEntered;
+                floor.OnFloorExited -= OnFloorExited;
+
+                foreach (var roomData in floor.RoomsInFloor)
+                {
+                    roomData.OnRoomEntered -= OnRoomEntered;
+                    roomData.OnRoomExited -= OnRoomExited;
+                }
+            }
+        }
+
+
+        private void Update()
+        {
+            if(_willGetDestroyed)
+                return;
+            
+            if (Player != null)
+            {
+                UpdateRoomsInPlayerRange();
+            }
+            //called explicitly to ensure that timer starts on same frame
+            RoomLoader.UpdateRoomUnloadTimer();
+        }
+
+
+        private void UpdateRoomsInPlayerRange()
+        {
+            if (_currentEnteredRoom == null)
+                return;
+            if (CurrentFloor == null)
+                return;
+            foreach (var roomData in CurrentFloor.RoomsInFloor)
+            {
+                if (roomData.IsPosInLoadingRange(Player.transform.position) ||
+                    CurrentEnteredRoom == roomData)
+                {
+                    HandleRoomEnteredLoadingRange(roomData);
+                }
+                else
+                {
+                    HandleRoomExitedLoadingRange(roomData);
+                }
+            }
+        }
+
+
+        public void SetRoomAsMustLoad(RoomData room)
+        {
+            AddRoomExteriorFlag(room, RoomFlag.IsGeneralMustLoad);
+            AddRoomInteriorFlag(room, RoomFlag.IsGeneralMustLoad);
+        }
+
+        public void UnsetRoomAsMustLoad(RoomData room)
+        {
+            RemoveExteriorRoomFlag(room, RoomFlag.IsGeneralMustLoad);
+            RemoveInteriorRoomFlag(room, RoomFlag.IsGeneralMustLoad);
+        }
+
+        public void RemoveExteriorRoomFlag(RoomData room, RoomFlag flags)
+        {
+            _roomLoader.RemoveRoomExteriorLoadFlag(room, flags);
+        }
+        
+        public void RemoveInteriorRoomFlag(RoomData room, RoomFlag flags)
+        {
+            _roomLoader.RemoveRoomInteriorLoadFlag(room, flags);
+        }
+
+        public void HandleRoomEnteredLoadingRange(RoomData room)
+        {
+            AddRoomExteriorFlag(room, RoomFlag.IsInLoadingRange);
+        }
+
+        public void HandleRoomExitedLoadingRange(RoomData room)
+        {
+            //actually unloading the room requries waiting for timer
+            //handled in different function
+            _roomLoader.RemoveRoomExteriorLoadFlag(room, RoomFlag.IsInLoadingRange);
+        }
+
+        RoomLoadHandle AddRoomExteriorFlag(RoomData room, RoomFlag flags)
+        {
+            return _roomLoader.AddExteriorLoadRequest(new RoomLoadRequestData(room), flags);
+        }
+        
+        internal async UniTask AddRoomExteriorFlagAndWait(RoomData room, RoomFlag flags)
+        {
+            var handle = _roomLoader.AddExteriorLoadRequest(new RoomLoadRequestData(room), flags);
+            await handle.LoadScene();
+        }
+        
+        internal async UniTask AddRoomInteriorFlagAndWait(RoomData room, RoomFlag flags)
+        {
+            var handle = _roomLoader.AddInteriorLoadRequest(new RoomLoadRequestData(room), flags);
+            await handle.LoadScene();
+        }
+        
+        RoomLoadHandle AddRoomInteriorFlag(RoomData room, RoomFlag flags)
+        {
+            return _roomLoader.AddInteriorLoadRequest(new RoomLoadRequestData(room), flags);
+        }
+
+        
+
+        public async UniTask EnterRoom(RoomData room, bool forceLoadIfMissing = false)
+        {
+            if (_currentEnteredRoom == null && !_roomLoader.RoomInteriorLoadHandles.ContainsKey(room) && !forceLoadIfMissing)
+            {
+                //the room has been entered but the exterior isn't marked as loaded
+                //this is possible if we start in this scene from the editor
+                //in which case, exterior is already loaded.
+                //we just need to add a dummy handle
+                //that won't unload the scene as an addressable.
+                _roomLoader.AddHandleForAlreadyLoadedInterior(room, RoomFlag.IsCurrentRoom);
+            }
+
+            Debug.Log("enter room "+ room, room);
+            if (_currentEnteredRoom != room)
+            {
+                var prevFloor = CurrentFloor;
+                var prevRoom = _currentEnteredRoom;
+
+                _currentEnteredRoom = room;
+                bool isDifferentFloor = prevFloor != _currentEnteredRoom.Floor;
+
+                if (prevRoom != null)
+                {
+                    ExitRoom(prevRoom);
+                    if (isDifferentFloor && prevFloor != null)
+                    {
+                        ExitFloor(prevFloor);
+                    }
+                }
+                
+
+                ForceEnterRoom(room);
+                await AddRoomExteriorFlagAndWait(room,RoomFlag.IsCurrentRoom);
+                await AddRoomInteriorFlagAndWait(room,RoomFlag.IsCurrentRoom);
+                OnRoomEntered?.Invoke(room);
+
+                LoadCurrentRoomDependencies(room, isDifferentFloor);
+            }
+        }
+
+        private void ExitFloor(FloorData prevFloor)
+        {
+            Debug.Log("exit floor " + prevFloor, prevFloor);
+            OnFloorExited?.Invoke(prevFloor);
+
+            foreach (var roomToUnload in prevFloor.RoomsInFloor)
+            {
+                RemoveExteriorRoomFlag(roomToUnload, RoomFlag.IsInLoadingRange);
+            }
+            
+            foreach (var roomToUnload in prevFloor.AlwaysLoadRooms)
+            {
+                RemoveExteriorRoomFlag(roomToUnload, RoomFlag.IsCurrentFloorMustLoad);
+            }
+        }
+
+        private void ExitRoom(RoomData prevRoom)
+        {
+            prevRoom.HandleRoomExited();
+            RemoveExteriorRoomFlag(prevRoom, RoomFlag.IsCurrentRoom);
+            RemoveInteriorRoomFlag(prevRoom, RoomFlag.IsCurrentRoom);
+            
+            foreach (var roomToUnload in prevRoom.AlwaysLoadRooms)
+            {
+                // note:
+                // if this is the only flag, this can start an unload
+                // say, the next room/floor has this room as an AlwaysLoadRoom
+                // then the room would be loaded again
+                // this is only in the edge case where the timer runs out between swapping rooms
+                // so not fixing atm
+                RemoveExteriorRoomFlag(roomToUnload, RoomFlag.IsCurrentFloorMustLoad);
+            }
+        }
+
+        private async UniTask LoadCurrentRoomDependencies(RoomData room, bool floorNewlyEntered)
+        {
+            //NOTE:FLAGS are set after the previous room is loaded
+            //it is not syncronous
+            //a better way would be to set all the flags and then wait on the handles
+            foreach (var adjacentRoom in room.AlwaysLoadRooms)
+            {
+                await AddRoomExteriorFlagAndWait(adjacentRoom, RoomFlag.IsCurrentFloorMustLoad);
+            }
+    
+            if (floorNewlyEntered)
+            {
+                if (room.Floor != null)
+                {
+                    OnFloorEntered?.Invoke(room.Floor);
+                    foreach (var roomToLoad in room.Floor.AlwaysLoadRooms)
+                    {
+                        await AddRoomExteriorFlagAndWait(roomToLoad, RoomFlag.IsCurrentFloorMustLoad);
+                    }
+                }
+            }
+            
+            room.HandleRoomDependenciesLoaded();
+        }
+
+
+        
+        /// <summary>
+        /// Returns if the exterior for the room is loaded
+        /// NOTE: For rooms with invalid exterior scene asset ref, this returns false
+        /// </summary>
+        /// <param name="roomData"></param>
+        /// <returns></returns>
+        public bool IsRoomExteriorLoaded(RoomData roomData)
+        {
+            if (_roomLoader.RoomExteriorLoadHandles.TryGetValue(roomData, out var handle))
+            {
+                return handle.IsLoaded();
+            }
+
+            return false;
+        }
+        
+
+
+        /// <summary>
+        /// Returns if the exterior for the room is loaded
+        /// NOTE: For rooms with invalid exterior scene asset ref, this returns false
+        /// </summary>
+        /// <param name="roomData"></param>
+        /// <returns></returns>
+        public bool IsRoomInteriorLoaded(RoomData roomData)
+        {
+            if (_roomLoader.RoomInteriorLoadHandles.TryGetValue(roomData, out var handle))
+            {
+                return handle.IsLoaded();
+            }
+
+            return false;
+        }
+        
+        public async UniTask UnloadAllRooms()
+        {
+            await _roomLoader.UnloadAllRooms();
+        }
+        
+
+        [Button]
+        void PrintRoomsInLoadingRange()
+        {
+            foreach (var (room, handle) in _roomLoader.RoomExteriorLoadHandles)
+            {
+                if (handle.HasFlag(RoomFlag.IsInLoadingRange))
+                {
+                    Debug.Log($"{room} in loading range");
+                }
+            }
+        }
+        [Button]
+        void PrintRoomsInMustLoad()
+        {
+            foreach (var (room, handle) in _roomLoader.RoomExteriorLoadHandles)
+            {
+                if (handle.HasFlag(RoomFlag.IsGeneralMustLoad))
+                {
+                    Debug.Log($"{room} is GeneralMustLoad");
+                }
+                if (handle.HasFlag(RoomFlag.IsCurrentRoomMustLoad))
+                {
+                    Debug.Log($"{room} is Current room MustLoad");
+                }
+                if (handle.HasFlag(RoomFlag.IsCurrentFloorMustLoad))
+                {
+                    Debug.Log($"{room} is Current floor MustLoad");
+                }
+            }
+        }
+
+        private void ForceEnterRoom(RoomData room)
+        {
+            _currentEnteredRoom = room;
+            room.HandleRoomEntered();
+        }
+    }
+}
---
+++ Samples/AddressableChunkLoaderSystem.Sample1/Scripts/Door.cs
@@ -0,0 +1,88 @@
+using System;
+using Bdeshi.Helpers.Utility;
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.AddressableChunkLoaderSystem.Core;
+using Studio23.SS2.AddressableChunkLoaderSystem.Data;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Sample1
+{
+    public class Door:MonoBehaviour
+    {
+        private Renderer _renderer;
+        [FormerlySerializedAs("innerRoom")] 
+        public RoomData InnerRoom;
+        [FormerlySerializedAs("outerRoom")] 
+        public RoomData OuterRoom;
+
+        public float OffsetDist = 1.5f;
+        public Vector3 OuterPoint => transform.position +  transform.right *OffsetDist;
+        public Vector3 InnerPoint => transform.position +  -transform.right *OffsetDist;
+        private Vector3 _ogPos;
+        public Vector3 OpeningOffset = Vector3.up * 4;
+        public Material HoverMat;
+        public Material NormalMat;
+        
+
+        private void Awake()
+        {
+            _ogPos = transform.position;
+            _renderer = GetComponent<Renderer>();
+        }
+
+        public void HandleInteractHoverStart()
+        {
+            _renderer.sharedMaterial = HoverMat;
+        }
+        
+        public void HandleInteractHoverEnd()
+        {
+            _renderer.sharedMaterial = NormalMat;
+        }
+
+        public async UniTask DoorOpenAnim()
+        {
+            FiniteTimer openAnimDuration = new FiniteTimer(.6f);
+            while (!openAnimDuration.isComplete)
+            {
+                openAnimDuration.updateTimer(Time.deltaTime);
+                transform.position = _ogPos + OpeningOffset * openAnimDuration.Ratio;
+
+                await UniTask.Yield();
+            }
+        }
+        
+        public async UniTask DoorCloseAnim()
+        {
+            transform.position = _ogPos;
+            await UniTask.Yield();
+        }
+
+        public Vector3 GetPosAfterDoorOpen()
+        {
+            var positionAfterEntry = RoomManager.Instance.CurrentEnteredRoom == InnerRoom
+                ? OuterPoint
+                : InnerPoint;
+            return positionAfterEntry;
+        }
+
+        public async UniTask Open()
+        {
+            var roomToLoad = RoomManager.Instance.CurrentEnteredRoom == InnerRoom
+                ? OuterRoom
+                : InnerRoom;
+            await RoomManager.Instance.EnterRoom(roomToLoad);
+            await DoorOpenAnim();
+        }        
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(OuterPoint, Vector3.up * 6); 
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(InnerPoint, Vector3.up * 6); 
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, transform.right * 6); 
+        }
+    }
+}

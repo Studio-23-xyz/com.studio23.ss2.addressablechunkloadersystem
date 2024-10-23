cf4b3f1476d82747d61b03b8d1d8e09e11b9071e
B-DeshiDev <ffalsoy@gmail.com>
2024-10-23 17:51:54 +0600

    allow swapping out roomset

---
+++ Editor/RoomMemoryInstanceEditor.cs
@@ -0,0 +1,250 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using Studio23.SS2.AddressableChunkLoaderSystem.Core;
+using Studio23.SS2.AddressableChunkLoaderSystem.Core.RoomMemory;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Editor
+{
+    public class RoomInstanceMemorySaverEditorWindow : EditorWindow
+    {
+        private static RoomInstanceMemorySaver _selectedSaver;
+        private TreeViewState treeViewState;
+        private IRoomMemoryTreeView roomMemoryTreeView;
+        
+        
+        [MenuItem("Studio-23/AddressableChunkLoader/RoomInstanceMemorySaver EditorWindow")]
+        public static void ShowWindow()
+        {
+            GetWindow<RoomInstanceMemorySaverEditorWindow>("Room Instance Memory Saver EditorWindow");
+        }
+
+        private void OnEnable()
+        {
+            // Search for RoomInstanceMemorySaver in the scene and set the static field
+            _selectedSaver = FindObjectOfType<RoomInstanceMemorySaver>();
+            if (treeViewState == null)
+                treeViewState = new TreeViewState();
+
+            roomMemoryTreeView = new IRoomMemoryTreeView(treeViewState);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Room Instance Memory Saver", EditorStyles.boldLabel);
+
+            // ObjectField for selecting a RoomInstanceMemorySaver
+            var newSaver = (RoomInstanceMemorySaver)EditorGUILayout.ObjectField("Room Instance Memory Saver",
+                _selectedSaver, typeof(RoomInstanceMemorySaver), true);
+            
+            if (roomMemoryTreeView == null)
+            {
+                roomMemoryTreeView = new IRoomMemoryTreeView(treeViewState);
+            }
+            if (_selectedSaver != newSaver)
+            {
+                roomMemoryTreeView.Reload();
+                _selectedSaver = newSaver;
+            }
+            if (GUILayout.Button("Update Selected RoomInstanceMemorySaver GUID "))
+            {
+                ForceGenerateUniqueIDs(_selectedSaver);
+            }
+
+            if (GUILayout.Button("Update All RoomInstanceMemorySaver GUID"))
+            {
+                GenerateForAllSaversInLoadedScenes();
+            }
+
+            var defualtGuiColor = GUI.color;
+            GUI.color = Color.yellow;
+            if (GUILayout.Button("Reload"))
+            {
+                roomMemoryTreeView.Reload();
+            }
+            GUI.color = Color.red;
+            if (GUILayout.Button("Delete all room memory "))
+            {
+                RoomMemorySaver.ClearAllMemory();
+            }
+            GUI.color = defualtGuiColor;
+            
+            roomMemoryTreeView.OnGUI(new Rect(0, 120, position.width, position.height - 180));
+        }
+
+        
+        public void ForceGenerateUniqueIDs(RoomInstanceMemorySaver saver)
+        {
+            HashSet<string> usedGUIDSet = new();
+            foreach (var roomMemory in saver.GetRoomMemoriesInRoom())
+            {
+                string id = roomMemory.ID;
+                while (string.IsNullOrEmpty(id) || usedGUIDSet.Contains(id))
+                {
+                    id = GUID.Generate().ToString();
+                    // Debug.Log($"{roomMemory} id {id} {usedGUIDSet.Contains(id)}",roomMemory.gameObject);
+                }
+
+                usedGUIDSet.Add(id);
+                
+                if (roomMemory.ID != id)
+                {
+                    roomMemory.ID = id;
+                    Debug.Log($"{roomMemory} set id {roomMemory.ID}-> {id}");
+                    EditorUtility.SetDirty(saver);
+                    EditorUtility.SetDirty(saver.gameObject);
+                }
+            }
+        }
+        
+        public static void ForceUpdateUniqueID(RoomInstanceMemorySaver saver, IRoomMemory roomMemory)
+        {
+            saver.ClearRoomIDs();
+            
+            string id = roomMemory.ID;
+            while (string.IsNullOrEmpty(id) || saver.IDToCountMap.ContainsKey(id))
+            {
+                id = GUID.Generate().ToString();
+                // Debug.Log($"{roomMemory} id {id} {usedGUIDSet.Contains(id)}",roomMemory.gameObject);
+            }
+
+            Debug.Log($"{roomMemory} set id {roomMemory.ID}-> {id}");
+            roomMemory.ID = id;
+            saver.RegisterRoomMemoryUnderID(roomMemory, roomMemory.ID);
+            if (roomMemory is Component roomMemoryCompoent)
+            {
+                EditorUtility.SetDirty(roomMemoryCompoent);
+            }
+            EditorUtility.SetDirty(roomMemory.gameObject);
+        }
+        
+        private void GenerateForAllSaversInLoadedScenes()
+        {
+            var savers = new List<RoomInstanceMemorySaver>();
+
+            
+            int sceneCount = EditorSceneManager.sceneCount;
+            for (int i = 0; i < sceneCount; i++)
+            {
+                var scene = EditorSceneManager.GetSceneAt(i);
+
+                // Iterate through the root game objects of the scene
+                foreach (var rootObject in scene.GetRootGameObjects())
+                {
+                    savers.AddRange(rootObject.GetComponentsInChildren<RoomInstanceMemorySaver>(true));
+                }
+            }
+
+            foreach (var saver in savers)
+            {
+                ForceGenerateUniqueIDs(saver);
+            }
+        }
+    }
+    
+    // TreeView for displaying IRoomMemory components
+    public class IRoomMemoryTreeView : TreeView
+    {
+        private List<IRoomMemory> roomMemories = new List<IRoomMemory>();
+
+        public IRoomMemoryTreeView(TreeViewState state) : base(state)
+        {
+            Reload();
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {
+            var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
+
+            // Gather all IRoomMemory components across loaded scenes
+            var roomMemorySavers = FindAllIRoomMemorySavers();
+            roomMemories = new();
+            int id = 1;
+            if (roomMemorySavers.Count == 0)
+            {
+                root.AddChild(new TreeViewItem { id = id++, displayName = "NO ROOM MEMORY OBJECTS"});
+            }
+            foreach (var roomMemorySaver in roomMemorySavers)
+            {
+                var memories =  roomMemorySaver.GetRoomMemoriesInRoom();
+                foreach (var roomMemory in memories)
+                {
+                    var item = new TreeViewItem { id = id++, displayName = roomMemory.GetType().Name };
+                    root.AddChild(item);
+                    roomMemories.Add(roomMemory);
+                }
+            }
+
+            return root;
+        }
+
+        protected override void RowGUI(RowGUIArgs args)
+        {
+            var roomMemory = roomMemories[args.item.id - 1];
+            var duplicate = roomMemory.Saver.isDuplicateRoomMemory(roomMemory);
+
+            Rect rowRect = args.rowRect;
+
+            // Highlight the row red if there are multiple occurrences of the ID
+            if (duplicate)
+            {
+                EditorGUI.DrawRect(rowRect, new Color(1f, 0.5f, 0.5f)); // Light red
+            }
+
+            // Display the type, GameObject name, and buttons
+            var x = rowRect.x;
+            var width = 200;
+            EditorGUI.LabelField(new Rect(rowRect.x, rowRect.y, width, rowRect.height), roomMemory.GetType().Name);
+            x += width;
+            
+            EditorGUI.LabelField(new Rect(x, rowRect.y, width, rowRect.height), roomMemory.gameObject.name);
+            x += width;
+            
+            EditorGUI.LabelField(new Rect(x, rowRect.y, width, rowRect.height), roomMemory.ID);
+            x += width;
+            // Button to log the IRoomMemory name
+            if (GUI.Button(new Rect(x, rowRect.y, width, rowRect.height), "add new ID"))
+            {
+                RoomInstanceMemorySaverEditorWindow.ForceUpdateUniqueID(roomMemory.Saver, roomMemory);
+            }
+
+            x += width;
+            // Button to open the file if it exists
+            if (File.Exists(roomMemory.GetPath()) && 
+                GUI.Button(new Rect(x, rowRect.y, width, rowRect.height), $"open"))
+            {
+                Application.OpenURL(roomMemory.GetPath());
+            }
+        }
+
+        protected override void SingleClickedItem(int id)
+        {
+            base.SingleClickedItem(id);
+            var roomMemory = roomMemories[id - 1];
+            Selection.activeGameObject = roomMemory.gameObject;
+        }
+
+        private List<RoomInstanceMemorySaver> FindAllIRoomMemorySavers()
+        {
+            List<RoomInstanceMemorySaver> memories = new ();
+            foreach (var scene in SceneManager.GetAllScenes())
+            {
+                if (scene.isLoaded)
+                {
+                    var rootObjects = scene.GetRootGameObjects();
+                    foreach (var root in rootObjects)
+                    {
+                        var foundMemories = root.GetComponentsInChildren<RoomInstanceMemorySaver>();
+                        memories.AddRange(foundMemories);
+                    }
+                }
+            }
+            return memories;
+        }
+    }
+}
+
---
+++ Runtime/Core/RoomMemory/RoomMemorySaver.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Studio23.SS2.AddressableChunkLoaderSystem.Data;
+using UnityEngine;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Core.RoomMemory
+{
+    public class RoomMemorySaver:MonoBehaviour
+    {
+        /// <summary>
+        /// Generates unique save path for  roomMemory for a given room
+        /// This has the benefit of the ID only needing to be unique in the same room aka scene
+        /// </summary>
+        /// <param name="roomData"></param>
+        /// <param name="roomMemory"></param>
+        /// <returns></returns>
+        public static string GetRoomMemorySavePath(RoomData roomData, IRoomMemory roomMemory)
+        {
+            var dirPath = GetSaveDir();
+            System.IO.Directory.CreateDirectory(dirPath);
+            return Path.Combine(
+                dirPath,
+                $"{roomData.name}_{roomMemory.ID}.ff"
+                );
+        }
+
+        private static string GetSaveDir()
+        {
+            return Path.Combine(
+                Application.persistentDataPath,
+                "addressableChunkloader"
+            );
+        }
+
+        public void SaveAllMemoriesInRoom(RoomData room, List<IRoomMemory> roomMemories)
+        {
+            foreach (var roomMemory in roomMemories)
+            {
+                SaveRoomMemoryAsync(room,roomMemory);
+            }
+        }
+
+        private void SaveRoomMemoryAsync(RoomData roomToSaveUnder, IRoomMemory roomMemory)
+        {
+            var data = roomMemory.GetTempSaveData();
+            var path = GetRoomMemorySavePath(roomToSaveUnder,roomMemory);
+                
+            if (File.Exists(path)) File.Delete(path);
+            //The string is generated and has no dependency on the IRoomMemory afterwards
+            //so we can call writeAllTextAsync
+            File.WriteAllTextAsync(path, data);
+        }
+        private void Awake()
+        {
+            ClearAllMemory();
+        }
+
+        public void LoadAllMemoriesInRoom(RoomData room, List<IRoomMemory> roomMemories)
+        {
+            foreach (var roomMemory in roomMemories)
+            {
+                LoadMemory(room, roomMemory);
+            }
+        }
+
+        [ContextMenu("ClearAllMemory")]
+        public static  void ClearAllMemory()
+        {
+            var clearSuccess = false;
+            var path = GetSaveDir();
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+            Debug.Log($"Clear all room memory clearSuccess {clearSuccess}");
+        }
+        
+        public static void ForceClearSingleRoomMemory(IRoomMemory roomMemory)
+        {
+            var roomInstance = roomMemory.gameObject.GetComponentInParent<RoomInstance>(true);
+            var p = GetRoomMemorySavePath(roomInstance._room, roomMemory);
+            if (File.Exists(p))
+            {
+                File.Delete(p);
+            }
+        }
+
+
+        private void LoadMemory(RoomData room,IRoomMemory roomMemory)
+        {
+            var path = GetRoomMemorySavePath(room, roomMemory);
+
+            if (File.Exists(path))
+            {
+                var data = File.ReadAllText(path);
+                //IRoomMemory may get destroyed by the time we call this
+                if (roomMemory != null)
+                {
+                    roomMemory.TempLoadData(data);
+                }
+            }
+        }
+    }
+}

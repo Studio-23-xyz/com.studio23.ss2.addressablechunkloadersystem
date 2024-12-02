aacf65c6f3a0081caa65db1df88e1c5b01eed00d
B-DeshiDev <ffalsoy@gmail.com>
2024-12-02 15:57:53 +0600

    Update package.json

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## 1.2.1
+Per room loading flag added
+
+## 1.0.1
+Moved samples to samples~
+
+## 1.0.1
+Dependency fix
+
+
+## 1.0.0
+Initial version of package
---
+++ Samples/AddressableChunkLoaderSystem.Sample1/Scripts/RoomMemoryTest.cs
@@ -0,0 +1,41 @@
+using System;
+using Bdeshi.Helpers.Utility;
+using Studio23.SS2.AddressableChunkLoaderSystem.Core.RoomMemory;
+using UnityEngine;
+
+namespace Studio23.SS2.AddressableChunkLoaderSystem.Sample1
+{
+    public class RoomMemoryTest:MonoBehaviour,IRoomMemory
+    {
+        public int EntryCount = 0;
+        public RoomInstanceMemorySaver Saver { get; set; }
+
+        public string ID
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+
+        [SerializeField] string _id;
+        public string GetTempSaveData()
+        {
+            return EntryCount.ToString();
+        }
+
+        public void TempLoadData(string data)
+        {
+            EntryCount = int.Parse(data);
+            Debug.Log($"After Loading EntryCount = {EntryCount}");
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                EntryCount++;
+                UnityEngine.Debug.Log($"ENtry count now {EntryCount}");
+            }
+        }
+    }
+}
